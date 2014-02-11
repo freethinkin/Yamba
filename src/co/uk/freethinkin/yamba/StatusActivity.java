f2532d49789417a786521569c6fe3d817985fc3a
freethinkin <andy@freethinkin.co.uk>
2014-02-11 18:14:27 +0000

    Added Threading

--- src/co/uk/freethinkin/yamba/StatusActivity.java
+++ src/co/uk/freethinkin/yamba/StatusActivity.java
@@ -1,13 +1,17 @@
 package co.uk.freethinkin.yamba;
 
 import android.app.Activity;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
-import 
+import android.widget.Toast;
+import winterwell.jtwitter.Twitter;
+import winterwell.jtwitter.TwitterException;
 
 public class StatusActivity extends Activity implements OnClickListener{
 	
@@ -22,11 +26,42 @@ public class StatusActivity extends Activity implements OnClickListener{
 		setContentView(R.layout.activity_status);
 		
 		// Find views
-		editText = (EditText) findViewById(R.id.editText); //
+		editText = (EditText) findViewById(R.id.editText); 
 		updateButton = (Button) findViewById(R.id.buttonUpdate);
-		updateButton.setOnClickListener(this); //
-		twitter = new Twitter("student", "password"); //
+		
+		updateButton.setOnClickListener(this); 
+		
+		twitter = new Twitter("student", "password"); 
 		twitter.setAPIRootUrl("http://yamba.marakana.com/api");
+	}
+		
+	// Asynchronously posts to twitter
+	class PostToTwitter extends AsyncTask<String, Integer, String> { //
+	// Called to initiate the background activity
+		@Override
+		protected String doInBackground(String... statuses) { //
+			try {
+				Twitter.Status status = twitter.updateStatus(statuses[0]);
+				return status.text;
+			} catch (TwitterException e) {
+				Log.e(TAG, e.toString());
+				e.printStackTrace();
+				return "Failed to post";
+			}
+		}
+		
+		// Called when there's a status to be updated
+		@Override
+		protected void onProgressUpdate(Integer... values) { //
+			super.onProgressUpdate(values);
+			// Not used in this case
+		}
+		
+		// Called once the background activity has completed
+		@Override
+		protected void onPostExecute(String result) { //
+			Toast.makeText(StatusActivity.this, result, Toast.LENGTH_LONG).show();
+		}
 	}
 
 	@Override
@@ -36,11 +71,11 @@ public class StatusActivity extends Activity implements OnClickListener{
 		return true;
 	}
 	
-
-	}
 	// Called when button is clicked //
 	public void onClick(View v) {
-	twitter.setStatus(editText.getText().toString()); //
-	Log.
+		String status = editText.getText().toString();
+		new PostToTwitter().execute(status); //
+		Log.d(TAG, "onClicked");
+	}
 
 }
