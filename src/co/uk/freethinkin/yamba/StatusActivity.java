bea9e4f6c4fcc3e7bb9b40ec862dc5195aeb0f24
freethinkin <andy@freethinkin.co.uk>
2014-02-11 21:14:13 +0000

    Added prefs and menu

--- src/co/uk/freethinkin/yamba/StatusActivity.java
+++ src/co/uk/freethinkin/yamba/StatusActivity.java
@@ -1,24 +1,30 @@
 package co.uk.freethinkin.yamba;
 
+import winterwell.jtwitter.Twitter;
+import winterwell.jtwitter.TwitterException;
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
-import winterwell.jtwitter.Twitter;
-import winterwell.jtwitter.TwitterException;
 
-public class StatusActivity extends Activity implements OnClickListener{
+public class StatusActivity extends Activity implements OnClickListener, TextWatcher{
 	
 	private static final String TAG = "StatusActivity";
 	EditText editText;
 	Button updateButton;
 	Twitter twitter;
+	TextView textCount;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -28,18 +34,38 @@ public class StatusActivity extends Activity implements OnClickListener{
 		// Find views
 		editText = (EditText) findViewById(R.id.editText); 
 		updateButton = (Button) findViewById(R.id.buttonUpdate);
-		
 		updateButton.setOnClickListener(this); 
+		
+		textCount = (TextView) findViewById(R.id.textCount); //
+		textCount.setText(Integer.toString(140)); //
+		textCount.setTextColor(Color.GREEN);
+		editText.addTextChangedListener(this);
 		
 		twitter = new Twitter("student", "password"); 
 		twitter.setAPIRootUrl("http://yamba.marakana.com/api");
 	}
+	
+	// Called first time user clicks on the menu button
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		MenuInflater inflater = getMenuInflater(); 
+		inflater.inflate(R.menu.menu, menu); 
+		return true; //
+	}
+	
+	// Called when button is clicked 
+		public void onClick(View v) {
+			String status = editText.getText().toString();
+			new PostToTwitter().execute(status); //
+			Log.d(TAG, "onClicked");
+		}
+
 		
 	// Asynchronously posts to twitter
-	class PostToTwitter extends AsyncTask<String, Integer, String> { //
+	class PostToTwitter extends AsyncTask<String, Integer, String> { 
 	// Called to initiate the background activity
 		@Override
-		protected String doInBackground(String... statuses) { //
+		protected String doInBackground(String... statuses) { 
 			try {
 				Twitter.Status status = twitter.updateStatus(statuses[0]);
 				return status.text;
@@ -52,30 +78,36 @@ public class StatusActivity extends Activity implements OnClickListener{
 		
 		// Called when there's a status to be updated
 		@Override
-		protected void onProgressUpdate(Integer... values) { //
+		protected void onProgressUpdate(Integer... values) { 
 			super.onProgressUpdate(values);
 			// Not used in this case
 		}
 		
 		// Called once the background activity has completed
 		@Override
-		protected void onPostExecute(String result) { //
+		protected void onPostExecute(String result) { 
 			Toast.makeText(StatusActivity.this, result, Toast.LENGTH_LONG).show();
 		}
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.status, menu);
-		return true;
+	// TextWatcher methods
+	public void afterTextChanged(Editable statusText) { 
+		int count = 140 - statusText.length(); 
+	
+		textCount.setText(Integer.toString(count));
+		textCount.setTextColor(Color.GREEN); 
+	
+		if (count < 10)
+			textCount.setTextColor(Color.YELLOW);
+		if (count < 0)
+			textCount.setTextColor(Color.RED);
 	}
 	
-	// Called when button is clicked //
-	public void onClick(View v) {
-		String status = editText.getText().toString();
-		new PostToTwitter().execute(status); //
-		Log.d(TAG, "onClicked");
+	public void beforeTextChanged(CharSequence s, int start, int count, int after) { 
 	}
-
+	
+	public void onTextChanged(CharSequence s, int start, int before, int count) { 
+	}
+	
+	
 }
