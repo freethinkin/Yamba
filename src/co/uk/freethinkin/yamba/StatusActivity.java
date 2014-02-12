b983af272110cfc67cdb0088b20e62d36e0c8cd8
freethinkin <andy@freethinkin.co.uk>
2014-02-12 19:04:57 +0000

    Threaded the network updater

--- src/co/uk/freethinkin/yamba/StatusActivity.java
+++ src/co/uk/freethinkin/yamba/StatusActivity.java
@@ -57,9 +57,15 @@ public class StatusActivity extends Activity implements OnClickListener, TextWat
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
 		switch (item.getItemId()) { 
+			case R.id.itemServiceStart:
+				startService(new Intent(this, UpdaterService.class)); 
+				break;
+			case R.id.itemServiceStop:
+				stopService(new Intent(this, UpdaterService.class)); 
+				break;
 			case R.id.itemPrefs:
 				startActivity(new Intent(this, PrefsActivity.class)); 
-			break;
+				break;
 		}
 		
 		return true; 
--- src/co/uk/freethinkin/yamba/UpdaterService.java
+++ src/co/uk/freethinkin/yamba/UpdaterService.java
@@ -7,6 +7,10 @@ import android.util.Log;
 
 public class UpdaterService extends Service{
 	static final String TAG = "UpdaterService";
+	
+	static final int DELAY = 60000; // a minute
+	private boolean runFlag = false; //
+	private Updater updater;
 
 	@Override
 	public IBinder onBind(Intent arg0) {
@@ -17,6 +21,7 @@ public class UpdaterService extends Service{
 	@Override
 	public void onCreate() {
 		super.onCreate();
+		this.updater = new Updater();
 		Log.d(TAG, "onCreated");
 	}
 
@@ -29,8 +34,37 @@ public class UpdaterService extends Service{
 	@Override
 	public int onStartCommand(Intent intent, int flags, int startId) {
 		super.onStartCommand(intent, flags, startId);
+		this.runFlag = true;
+		this.updater.start();
 		Log.d(TAG, "onStarted");
+		
 		return START_STICKY;
 	}
+	
+	/**
+	* Thread that performs the actual update from the online service
+	*/
+	private class Updater extends Thread { 
+		
+		public Updater() {
+			super("UpdaterService-Updater"); 
+		}
+		
+		@Override
+		public void run() { 
+			UpdaterService updaterService = UpdaterService.this; 
+		
+			while (updaterService.runFlag) { 
+				Log.d(TAG, "Updater running");
+				try {
+					// Some work goes here...
+					Log.d(TAG, "Updater ran");
+					Thread.sleep(DELAY); //
+				} catch (InterruptedException e) { //
+					updaterService.runFlag = false;
+				}
+			}
+		}
+	} // Updater
 
 }
